e0ba63ea13df7d74c9c99f13b7b73eb05f56b4f8
Manfred Steyer <manfred.steyer@gmx.net>
2020-08-10 16:32:44 +0200

    +performance, +animation for landscape

--- engine.js
+++ engine.js
@@ -37,17 +37,6 @@ const areasCube = [
     [5,1,8]
 ];
 
-// const coordsCube = [
-//     toPoint3d(1, 1, 5),
-//     toPoint3d(4, 1, 5),
-//     toPoint3d(3, 4, 5),
-// ];
-
-// const areasCube = [
-//    [1,2,3]
-// ];
-
-
 const rotation = {
     ay: 0,
     ax: 0
@@ -128,7 +117,6 @@ function intersectPlane(plane, lineStartPoint3d, lineEndPoint3d) {
     planeNormal = normalize(planeNormal);
 
     // dotProduct with normal is vector length when projected to normal
-    // http://www.mathematrix.de/skalarprodukt/
     const planeDot = dotProduct(planeNormal, planePoint);
     const ad = dotProduct(lineStartPoint3d, planeNormal);
     const bd = dotProduct(lineEndPoint3d, planeNormal);
@@ -145,11 +133,6 @@ function clip(plane, triple) {
     const insidePoints = [];
     const outsidePoints = [];
 
-    // TODO: Repesent triple by an object, not by an array
-
-    // const planeNormal = normalize(plane.planeNormal);
-    // const planePoint = plane.planePoint;
-
     plane = { 
         planeNormal: normalize(plane.planeNormal),
         planePoint: plane.planePoint
@@ -158,11 +141,6 @@ function clip(plane, triple) {
     const { planeNormal, planePoint } = plane;
 
     const distance = (point3d) => {
-        //const result3 = planeNormal.x * point3d.x + planeNormal.y * point3d.y + planeNormal.z * point3d.z - dotProduct(planeNormal, planePoint);
-        
-        // https://mathinsight.org/distance_point_plane#:~:text=The%20length%20of%20the%20gray,dot%20product%20v%E2%8B%85n.
-        //const result = planeNormal.x * (point3d.x - planePoint.x) + planeNormal.y * (point3d.y - planePoint.y) + planeNormal.z * (point3d.z - planePoint.z);
-        
         // dotProduct with normal: length of vector when projected to normal
         const result = dotProduct(point3d, planeNormal) - dotProduct(planePoint, planeNormal);
 
@@ -189,7 +167,6 @@ function clip(plane, triple) {
     if (insidePoints.length === 3) {
         return [triple];
     }
-
 
     if (insidePoints.length === 1) {
         let a =             [
@@ -227,18 +204,6 @@ function clip(plane, triple) {
 
 // https://github.com/OneLoneCoder/videos/blob/master/OneLoneCoder_olcEngine3D_Part2.cpp
 function crossProduct(point3d_1, point3d_2, point3d_3) {
-    // const line1 = {
-    //     x: point3d_2.x - point3d_1.x,
-    //     y: point3d_2.y - point3d_1.y,
-    //     z: point3d_2.z - point3d_1.z,
-    // };
-
-    // const line2 = {
-    //     x: point3d_3.x - point3d_1.x,
-    //     y: point3d_3.y - point3d_1.y,
-    //     z: point3d_3.z - point3d_1.z,
-    // };
-
     const line1 = substract(point3d_2, point3d_1);
     const line2 = substract(point3d_3, point3d_1);
 
@@ -263,22 +228,41 @@ function normalize(point3d) {
     return result;
 }
 
-function load(parsedFile, animate) {
+let objType;
+let minZ;
+
+function load(parsedFile, type, x, y) {
     coords = parsedFile.coords;
     areas = parsedFile.areas;
 
     const max = Math.max( ...[...coords.map(c => c.x), ...coords.map(c => c.y), ...coords.map(c => c.z)] );
-    recommentedDistance = max + 2;
+    recommentedDistance = max + 4;
     camera.z = recommentedDistance;
+
+    minZ = Math.min(...coords.map(c => c.z));
+
+    if (y) {
+        camera.y = y;
+    }
+    else {
+        camera.y = 0;
+    }
+
+    if (x) {
+        camera.x = x;
+
+    }
+    else {
+        camera.x = 0;
+    }
+
     rotation.ax = 0;
     rotation.ay = 0;
+    objType = type;
 
     stopAnimation();
     render();
-
-    if (animate) {
-        startAnimation();
-    }
+    startAnimation();
 }
 
 function loadCube() {
@@ -313,7 +297,6 @@ function project(point3d) {
     return { x: bx, y: by };
 }
 
-
 function project3d(point3d) {
     const x = point3d.x - camera.x;
     const y = point3d.y - camera.y;
@@ -342,7 +325,6 @@ function project3d(point3d) {
 }
 
 function projected3dTo2d(point3d) {
-
     const ex = displaySurface.x;
     const ey = displaySurface.y;
     const ez = displaySurface.z;
@@ -371,12 +353,6 @@ function calcColor(point1, point2, point3) {
     // it doen't become blacker ...
     let intensity = Math.max(0, cosineSimilarity);
   
-    //const experiment = intensity / 2 + 0.5;
-    //intensity = experiment;
-
-    //console.assert(experiment >= 0 && experiment <= 1, experiment);
-    // console.assert(!isNaN(intensity));
-
     const code = Math.floor(intensity * 255);
     const rgba =  `rgba(0, ${code/2}, ${code}, 255)`;
 
@@ -390,107 +366,105 @@ function clearCanvas() {
 const ctx = document.getElementById('canvas').getContext('2d');
 
 function drawTriangle(p1_2d, p2_2d, p3_2d, color) {
-        ctx.fillStyle = color;
-        ctx.beginPath();
-        ctx.moveTo(p1_2d.x, p1_2d.y);
-        ctx.lineTo(p2_2d.x, p2_2d.y);
-        ctx.lineTo(p3_2d.x, p3_2d.y);
-        ctx.closePath();
-        ctx.fill();
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.moveTo(p1_2d.x, p1_2d.y);
+    ctx.lineTo(p2_2d.x, p2_2d.y);
+    ctx.lineTo(p3_2d.x, p3_2d.y);
+    ctx.closePath();
+    ctx.fill();
 }
+
+function isVisible(triple) {
+    const point1 = triple[0];
+    const point2 = triple[1];
+    const point3 = triple[2];
+
+    const normal = normalize(crossProduct(point1, point2, point3));
+
+    const x = point1.x;
+    const y = point1.y;
+    const z = point1.z;
+    
+    // Compare normal with vector -- < 0 --> < 90° 
+    return (normal.x * x + normal.y * y + normal.z * z) < 0;
+}
+
+function transform(coord) {
+    coord = rotateY(coord, rotation.ay);
+    coord = rotateX(coord, rotation.ax)
+    return coord;    
+}
+
+function clip2d(triple) {
+    let triples = [triple];
+
+    const leftPlane = {
+        planePoint: toPoint3d(0.0, 0.0, 0.0),
+        planeNormal: toPoint3d(0.0, 1.0, 0.0),
+    };
+
+    const rightPlane = {
+        planePoint: toPoint3d(0.0, 300-1, 0.0),
+        planeNormal: toPoint3d(0.0, -1.0, 0.0),
+    };
+
+    const topPlane = {
+        planePoint: toPoint3d(0.0, 0.0, 0.0),
+        planeNormal: toPoint3d(1.0, 0.0, 0.0),
+    };
+
+    const bottomPlane = {
+        planePoint: toPoint3d(300-1, 0.0, 0.0),
+        planeNormal: toPoint3d(-1.0, 0.0, 0.0),
+    };   
+
+    triples = triples.flatMap(triple => clip(topPlane, triple));
+    triples = triples.flatMap(triple => clip(bottomPlane, triple));
+    triples = triples.flatMap(triple => clip(leftPlane, triple));
+    triples = triples.flatMap(triple => clip(rightPlane, triple));
+    return triples;
+
+};
 
 function render() {
     clearCanvas();
 
-
-    const coordsTest = coords.map(coord => {
-        return {
-            x: -coord.x,
-            y: -coord.y,
-            z: -coord.z
-        }
-    })
-
-    coords3dTransformed = coords.map(coord => {
-        coord = rotateY(coord, rotation.ay);
-        coord = rotateX(coord, rotation.ax)
-        return coord;
-    }); 
-
     const triples = areas.map(area => ([
-        coords3dTransformed[area[0]-1],
-        coords3dTransformed[area[1]-1],
-        coords3dTransformed[area[2]-1]
+        coords[area[0]-1],
+        coords[area[1]-1],
+        coords[area[2]-1]
     ]));
+    
+    const screenPlane = {
+        planePoint: toPoint3d(0, 0, -1.0),
+        planeNormal: toPoint3d(0, 0, -1),
+    };
 
-    // clippedTriples = triples;
-    // clippedTriples = triples;
-    // sortedTriples = clippedTriples;
+    const projTriples = triples.flatMap(triple => {
+        const p1 = transform(triple[0]);
+        const p2 = transform(triple[1]);
+        const p3 = transform(triple[2]);
 
-    let projTriples = triples.map(triple => {
-        const p1 = triple[0];
-        const p2 = triple[1];
-        const p3 = triple[2];
+        const p1_p = project3d(p1);
+        const p2_p = project3d(p2);
+        const p3_p = project3d(p3);
 
-        const p1_p = project3d(triple[0]);
-        const p2_p = project3d(triple[1]);
-        const p3_p = project3d(triple[2]);
+        let projected = [p1_p, p2_p, p3_p];
 
-        let result = [p1_p, p2_p, p3_p];
-        return result;
+        if (isVisible(projected)) {
+            return clip(screenPlane, projected);
+        }
+        return [];
     });
 
-    const filteredTriples = projTriples.filter(triple => {
-        const point1 = triple[0];
-        const point2 = triple[1];
-        const point3 = triple[2];
-
-        const normal = normalize(crossProduct(point1, point2, point3));
-
-        const x = point1.x;
-        const y = point1.y;
-        const z = point1.z;
-        
-        // Compare normal with vector -- < 0 --> < 90° 
-        return (normal.x * x + normal.y * y + normal.z * z) < 0;
-    });
-
-    let clippedTriples = filteredTriples.flatMap(triple => {
-
-        // const plane = {
-        //     planePoint: toPoint3d(0, 0, 0.1),
-        //     planeNormal: toPoint3d(0, 0, -3),
-        // };
-
-        const plane = {
-            planePoint: toPoint3d(0, 0, -1.0),
-            planeNormal: toPoint3d(0, 0, -1),
-        };
-
-        return clip(plane, triple);
-    });
-
-    // let filteredclippedTriples = clippedTriples.filter(triple => {
-    //     const point1 = triple[0];
-    //     const point2 = triple[1];
-    //     const point3 = triple[2];
-
-    //     const cross = normalize(crossProduct(point1, point2, point3));
-
-    //     const x = point1.x;
-    //     const y = point1.y;
-    //     const z = point1.z;
-
-    //     return (cross.x * x + cross.y * y + cross.z * z) < 0;
-    // });
-
-    clippedTriples = clippedTriples.sort((t1, t2) => {
+    const sorted = projTriples.sort((t1, t2) => {
         const z1 = (t1[0].z + t1[1].z + t1[2].z);
         const z2 = (t2[0].z + t2[1].z + t2[2].z);
         return z1 - z2;
     });
    
-    const canvasTriples = clippedTriples.map(triple => {
+    sorted.forEach(triple => {
         const color = calcColor(triple[0], triple[1], triple[2]); 
 
         const p1 = projected3dTo2d(triple[0]);
@@ -501,86 +475,13 @@ function render() {
         const c2 = toCanvasPoint(p2);
         const c3 = toCanvasPoint(p3);
 
-        let result = [c1,c2,c3];
-        result.color = color;
-        return result;
+        let triple2d = [c1,c2,c3];
+        triple2d.color = color;
 
+        clip2d(triple2d).forEach(triple => {
+            drawTriangle(triple[0], triple[1], triple[2], triple.color);
+        });
     });
-
-     const clippedCanvasTriples = canvasTriples.flatMap(triple => {
-        let triples = [triple];
-
-        const leftPlane = {
-            planePoint: toPoint3d(0.0, 0.0, 0.0),
-            planeNormal: toPoint3d(0.0, 1.0, 0.0),
-        };
-
-        const rightPlane = {
-            planePoint: toPoint3d(0.0, 300-1, 0.0),
-            planeNormal: toPoint3d(0.0, -1.0, 0.0),
-        };
-
-        const topPlane = {
-            planePoint: toPoint3d(0.0, 0.0, 0.0),
-            planeNormal: toPoint3d(1.0, 0.0, 0.0),
-        };
-
-        const bottomPlane = {
-            planePoint: toPoint3d(300-1, 0.0, 0.0),
-            planeNormal: toPoint3d(-1.0, 0.0, 0.0),
-        };   
-
-        triples = triples.flatMap(triple => clip(topPlane, triple));
-        triples = triples.flatMap(triple => clip(bottomPlane, triple));
-        triples = triples.flatMap(triple => clip(leftPlane, triple));
-        triples = triples.flatMap(triple => clip(rightPlane, triple));
-        return triples;
-
-    });
-
-    for (const triple of clippedCanvasTriples) {
-        drawTriangle(triple[0], triple[1], triple[2], triple.color);
-    }
-
-    // for (const triple of clippedTriples) {
-
-    //     const p1 = triple[0];
-    //     const p2 = triple[1];
-    //     const p3 = triple[2];
-
-    //     const p1_p = project(triple[0]);
-    //     const p2_p = project(triple[1]);
-    //     const p3_p = project(triple[2]);
-
-    //     const color = calcColor(p1, p2, p3);
-
-    //     const p1_2d = toCanvasPoint(p1_p);
-    //     const p2_2d = toCanvasPoint(p2_p);
-    //     const p3_2d = toCanvasPoint(p3_p);
-
-    //     drawTriangle(p1_2d, p2_2d, p3_2d, color);
-
-    // }
-
-    // TODO: Iterate clippedTriples
-    //      project to 2d triples
-    //      transform to canvas coordinates
-    //      draw triangle
-    // const coords2d = coords3dTransformed.map(coord => {
-    //     const point2d = project(coord);
-        
-    //     return toCanvasPoint(point2d);
-    // });
-
-    // for (area of filteredAreas) {
-    //     ctx.fillStyle = calcColor(area);
-    //     ctx.beginPath();
-    //     ctx.moveTo(coords2d[area[0]-1].x, coords2d[area[0]-1].y);
-    //     ctx.lineTo(coords2d[area[1]-1].x, coords2d[area[1]-1].y);
-    //     ctx.lineTo(coords2d[area[2]-1].x, coords2d[area[2]-1].y);
-    //     ctx.closePath();
-    //     ctx.fill();
-    // } 
 }
 
 function startAnimation() {
@@ -600,6 +501,15 @@ let time = Date.now();
 let animationFrame;
 
 function animate() {
+    if (!objType || objType === 'obj') {
+        animateObj();
+    }
+    else {
+        animateLandscape();
+    }
+}
+
+function animateObj() {
     if (!animationStarted) return;
 
     const now = Date.now();
@@ -607,6 +517,26 @@ function animate() {
 
     if (elapsed > 1/66) {
         rotation.ay += Math.PI / 180 * 2;
+        render();
+        time = now;
+    }
+
+    animationFrame = window.requestAnimationFrame(event => {
+        animate();        
+    });
+}
+
+
+
+function animateLandscape() {
+    if (!animationStarted) return;
+    if (camera.z <= minZ + 30) return;
+
+    const now = Date.now();
+    const elapsed = (now - time)/1000;
+
+    if (elapsed > 1/66) {
+        camera.z -= 0.2;
         render();
         time = now;
     }
@@ -630,26 +560,19 @@ function handleInput(key) {
         case 'ArrowDown':
             rotation.ax += Math.PI / 180 *2;
             break;
-        case '+':
-            camera.z += 0.4;
+      
+        case 'a':
+            camera.oy -= Math.PI / 180 *2;
             break;
-        case '-':
+        case 'd':
+            camera.oy += Math.PI / 180 *2;
+            break;
+        case 'w':
             camera.z -= 0.4;
             break;
-
-            case 'a':
-                camera.x -= 0.4;
-                break;
-            case 'd':
-                camera.x += 0.4;
-                break;
-
-            case 'w':
-                camera.y -= 0.4;
-                break;
-            case 's':
-                camera.y += 0.4;
-                break;            
+        case 's':
+            camera.z += 0.4;
+            break;            
 
         case '0':
             camera.z = recommentedDistance;
